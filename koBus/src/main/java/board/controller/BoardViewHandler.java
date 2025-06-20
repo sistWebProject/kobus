b34680361395e5032296fc26c7b6ec5010131f3a
seung <seung@C_Seung_H0>
2025-06-20 17:43:54 +0900

    핸들러 전부 수정

--- koBus/src/main/java/board/controller/BoardViewHandler.java
+++ koBus/src/main/java/board/controller/BoardViewHandler.java
@@ -3,23 +3,23 @@ package board.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import board.dao.noticeDAO;
-import board.dto.NoticeDTO;
+import board.dao.BoardDAO;
+import board.dto.BoardDTO;
 import koBus.mvc.command.CommandHandler;
 
-public class NoticeViewHandler implements CommandHandler {
+public class BoardViewHandler implements CommandHandler {
     @Override
     public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
     	request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
     	
     	System.out.println("VIEW_doit()..");
-    	String notID = request.getParameter("notID");
+    	String brdID = request.getParameter("brdID");
     	
-        noticeDAO dao = new noticeDAO();
-        NoticeDTO dto = dao.getNotice(notID);
+    	BoardDAO dao = new BoardDAO();
+        BoardDTO dto = dao.getBoard(brdID);
         
         request.setAttribute("dto", dto);
-        return "/notice/noticeView.jsp";
+        return "/board/boardView.jsp";
     }
 }
